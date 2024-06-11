21622606ed32ec5bb0487e2c156ae600ef35b3a6
Mono-Rim <augustooliveirasouza1@gmail.com>
2024-06-11 15:45:22 -0300

    introducao poo

---
+++ CursoPOO/src/Animais/Cachorro.java
@@ -0,0 +1,34 @@
+package Animais;
+
+public class Cachorro {
+
+    public String nome;
+    public String cor;
+    public int altura;
+    public double peso;
+    public int tamanhoDoRabo;
+    public String estadoDeEspirito;
+
+    public void comer(){
+
+    }
+
+    public void latir(){
+        System.out.println("Au Au Au Au Au Au Au");
+    }
+
+    public String pegar(){
+        return "Bolinha";
+    }
+
+    public String interagir(String acao) {
+        if (acao.equals("carinho")) {
+            this.estadoDeEspirito = "feliz";
+        } else if(acao.equals("vai dormir")){
+            this.estadoDeEspirito = "bravo";
+        }else{
+            this.estadoDeEspirito = "neutro";
+        }
+        return estadoDeEspirito;
+    }
+}
---
+++ CursoPOO/src/Main.java
@@ -0,0 +1,23 @@
+import Animais.Cachorro;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cachorro cachorro1 = new Cachorro();
+
+        cachorro1.nome = "Diana";
+        cachorro1.cor = "Azul";
+        cachorro1.altura = 25;
+        cachorro1.peso = 18.5;
+        cachorro1.tamanhoDoRabo = 5;
+
+        //cachorro1.latir();
+
+        //System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+
+        System.out.println("o cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("o cachorro está " + cachorro1.interagir("nada"));
+        System.out.println("o cachorro está " + cachorro1.interagir("vai dormir"));
+    }
+}
